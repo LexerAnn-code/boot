bfb27e4a11a53bb96f87696157fbed0cbbfa22ae
LexerAnn-code <kenannan005@gmail.com>
2020-10-27 01:45:16 -0300

    avail

--- src/main/java/combase/demo/Boot.java
+++ src/main/java/combase/demo/Boot.java
@@ -1,12 +1,32 @@
 package combase.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("base")
 public class Boot {
+    @PostMapping("/avail")
+    public JsonNode details(@RequestBody JsonNode jsonNode){
+        System.out.println("Received JSON:"+jsonNode.toString());
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<JsonNode> request = new HttpEntity<>(jsonNode);
+
+        String url = "https://safe-basin-01006.herokuapp.com/api/avail_spectrum";
+        ResponseEntity<JsonNode> response =
+                restTemplate.exchange(url,
+                        HttpMethod.POST,
+                        request,
+                        JsonNode.class);
+        JsonNode responsePayload = response.getBody();
+        return responsePayload;
+
+
+    }
     @GetMapping()
     public String boot(){
         return  "adasdasd";
